f239c7bdf192608760c63c9b0effc9eaf4c91d31
Enrique Naguiat <enzo_naguiat@yahoo.com>
2018-04-27 17:48:24 +0800

    creates order model, controller and integrates stripe

---
+++ app/controllers/orders_controller.rb
@@ -0,0 +1,18 @@
+class OrdersController < ApplicationController
+
+
+  def index
+    @orders = current_user.orders.where(state: 'paid')
+  end
+
+  def show
+    @order = current_user.orders.where(state: 'paid').find(params[:id])
+  end
+
+  def create
+    menu_item = MenuItem.find(params[:menu_item_id])
+    order = Order.create!(menu_item_sku: menu_item.sku, amount: menu_item.price, state: 'pending', user: current_user)
+
+    redirect_to new_order_payment_path(order)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180427082024) do
+ActiveRecord::Schema.define(version: 20180427084043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,17 @@ ActiveRecord::Schema.define(version: 20180427082024) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "price_cents", default: 0, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.string "state"
+    t.string "menu_item_sku"
+    t.integer "amount_cents", default: 0, null: false
+    t.jsonb "payment"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_orders_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -42,4 +53,5 @@ ActiveRecord::Schema.define(version: 20180427082024) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "orders", "users"
 end
