31e8bb445f39ec075823099beb30fe3b762ea074
Enrique Naguiat <enzo_naguiat@yahoo.com>
2018-05-01 17:47:49 +0800

    adds pundit policy to orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,15 +1,17 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = current_user.orders.where(state: 'paid')
+    @orders = policy_scope(Order).order(created_at: :desc)
   end
 
   def show
     @order = current_user.orders.where(state: 'paid').find(params[:id])
+    authorize @order
   end
 
   def create
     @order = Order.create(user: current_user, state: 'pending')
+    authorize @order
     redirect_to new_order_order_detail_path(@order)
   end
 end
---
+++ app/policies/order_policy.rb
@@ -0,0 +1,21 @@
+class OrderPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.where(state: 'paid', user: user)
+    end
+  end
+
+  def show?
+    user_is_owner_or_admin?
+  end
+
+  def create?
+    user_is_owner_or_admin?
+  end
+
+
+  private
+    def user_is_owner_or_admin?
+      record.user == user || user.admin
+    end
+end
