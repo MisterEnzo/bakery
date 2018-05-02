6bbedb8b14379df0c9272f1d2c62a074fd3c19d1
Enrique Naguiat <enzo_naguiat@yahoo.com>
2018-05-02 08:48:53 +0800

    add contact page and modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,15 @@
 Rails.application.routes.draw do
-  get 'order_details/new'
 
-  get 'order_details/create'
-
-  resources :posts
-  devise_for :users
   root to: 'pages#home'
   get '/about', to:'pages#about'
+  get '/contact', to:'pages#contact'
+
+  resources :posts
 
   resources :orders, only: [:index, :show, :create] do
     resources :payments, only: [:new, :create]
     resources :order_details, only: [:new, :create]
   end
+
+  devise_for :users
 end
